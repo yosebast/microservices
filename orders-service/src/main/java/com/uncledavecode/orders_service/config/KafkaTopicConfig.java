86c6f8b403325472a5ccece19669ff7f8e4a846f
yosebast <ik_sbtian@yahoo.es>
2024-04-24 22:49:10 +0200

    include new config kafka and mongo and mongo express

---
+++ orders-service/src/main/java/com/uncledavecode/orders_service/config/KafkaTopicConfig.java
@@ -0,0 +1,32 @@
+package com.uncledavecode.orders_service.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic generateTopic(){
+        Map<String, String> configurations = new HashMap<>();
+        configurations.put(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_DELETE);
+        configurations.put(TopicConfig.RETENTION_MS_CONFIG, "86400000");
+        configurations.put(TopicConfig.SEGMENT_BYTES_CONFIG, "1073741824");
+        configurations.put(TopicConfig.MAX_MESSAGE_BYTES_CONFIG, "1000012");
+
+        NewTopic newTopic = TopicBuilder.name("orders-topic2")
+                .partitions(2)
+                .replicas(2)
+                .configs(configurations)
+                .build();
+
+        return newTopic;
+
+    }
+}
